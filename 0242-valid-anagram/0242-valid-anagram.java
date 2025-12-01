3be91781d6f7f437c901fe86cfd9025d0be2b931
Md Akheel Baba <akheelbaba51@gmail.com>
2025-12-01 10:08:25 +0530

    Time: 4 ms (78.76%), Space: 44 MB (74.26%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int[] charCounts =new int[26];
+
+        for(int i=0;i<s.length();i++){
+            charCounts[s.charAt(i)- 'a']++;
+        }
+        for(int j=0;j<t.length();j++){
+            charCounts[t.charAt(j)- 'a']--;
+        }
+        for(int count:charCounts){
+            if(count!=0){
+                return false;
+            }
+        }
+        return true;
+
+        }
+    }
